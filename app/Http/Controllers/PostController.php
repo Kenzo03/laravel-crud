fce20e11f37481046abbd7a16e556d630f898211
Kenzo03 <kevinwijaya480@gmail.com>
2021-04-28 15:10:00 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = DB::table('posts')
+                ->leftjoin('categories','posts.category_id','=','categories.id')
+                ->select('posts.*','categories.name')
+                ->latest()
+                ->paginate(5);
+        // $data = Post::latest()->paginate(5);
+        return view('posts.index',compact('data'))
+                ->with('i',(request()->input('page',1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all(['id','name']);
+        return view('posts.create',compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'category_id' => 'required'
+        ]);
+
+        Post::create($request->all());
+
+        return redirect()->route('posts.index')
+                ->with('success','Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        $data = $post->load('category');
+        return view('posts.show',compact('data'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::all(['id','name']);
+        return view('posts.edit',compact('post','categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        $post->update($request->all());
+
+        return redirect()->route('posts.index')
+                         ->with('Success','Post updated successfully');
+    }           
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('posts.index')
+                         ->with('success','Post deleted successfully');
+    }
+}
---
+++ resources/views/layouts/blok.blade.php
@@ -0,0 +1,2 @@
+
+<p>Ini Footer</p>
